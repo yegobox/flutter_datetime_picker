a83b42140f994323c9d4d55b30f1d64e255a27c9
Richard Muragijimana <beastar457@gmail.com>
2023-05-12 18:28:59 +0200

    clean-up

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/hasan/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/hasan/Documents/flutter_projects/flutter_datetime_picker/example"
+export "FLUTTER_ROOT=C:\Users\SPENN\Documents\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\SPENN\Documents\flutter3.10.0\flipper\open-sources\flutter_datetime_picker\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
